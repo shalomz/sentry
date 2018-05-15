6640584ad2fdb9db6f6989791740861b60cd16a0
Lauryn Brown <lauryndbrown@gmail.com>
2018-05-14 17:36:41 -0700

    Added jwt split for jira and bitbucket.

--- src/sentry/integrations/jira/client.py
+++ src/sentry/integrations/jira/client.py
@@ -1,13 +1,9 @@
 from __future__ import absolute_import
 
-import datetime
-import jwt
 from six.moves.urllib.parse import urlparse
 
-from sentry.http import build_session
+from sentry.integrations.atlassian_connect import integration_request
 from sentry.utils.http import absolute_uri
-
-from .utils import get_query_hash
 
 JIRA_KEY = '%s.jira' % (urlparse(absolute_uri()).hostname, )
 
@@ -20,33 +16,8 @@ class JiraApiClient(object):
         self.base_url = base_url
         self.shared_secret = shared_secret
 
-    def request(self, method, path, data=None, params=None, headers=None, **kwargs):
-        jwt_payload = {
-            'iss': JIRA_KEY,
-            'iat': datetime.datetime.utcnow(),
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5 * 60),
-            'qsh': get_query_hash(path, method.upper(), params),
-        }
-        encoded_jwt = jwt.encode(jwt_payload, self.shared_secret)
-        params = dict(
-            jwt=encoded_jwt,
-            **(params or {})
-        )
-
-        session = build_session()
-        resp = session.request(
-            method.lower(),
-            url='%s%s' % (self.base_url, path),
-            headers=headers,
-            json=data,
-            params=params,
-        )
-
-        resp.raise_for_status()
-        return resp.json()
-
     def get_issue(self, issue_id):
-        return self.request('GET', self.ISSUE_URL % (issue_id,))
+        return integration_request('GET', self.ISSUE_URL % (issue_id,))
 
     def create_comment(self, issue_key, comment):
-        return self.request('POST', self.COMMENT_URL % issue_key, data={'body': comment})
+        return integration_request('POST', self.COMMENT_URL % issue_key, data={'body': comment})
